8431cc65fd838e7356f47e16797e1bacd29175c6
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-04-11 01:30:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'LISA-KOREA'
+def hello():
+    return 'Hello, World!'
 
-
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    # Run the Flask app on port 80
+    app.run(host='0.0.0.0', port=80)
