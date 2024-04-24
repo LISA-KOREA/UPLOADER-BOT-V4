71607346d1ea79de2d90dfa5d03f1ab7c47642bb
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-04-24 09:56:56 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -1,4 +1,4 @@
-# @Shrimadhav Uk
+# @Shrimadhav Uk | @LISA_FAN_LK
 
 
 import logging
@@ -13,12 +13,12 @@ import os
 import shutil
 import time
 from datetime import datetime
-from plugins.config import Config
-from plugins.script import Translation
-from plugins.thumbnail import *
-from plugins.database.database import db
+from Uploader.config import Config
+from Uploader.script import Translation
+from Uploader.thumbnail import *
+from Uploader.database.database import db
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from plugins.functions.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+from Uploader.functions.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
@@ -63,9 +63,7 @@ async def ddl_call_back(bot, update):
                 o = entity.offset
                 l = entity.length
                 youtube_dl_url = youtube_dl_url[o:o + l]
-    user = await bot.get_me()
-    mention = user["mention"]
-    description = Translation.CUSTOM_CAPTION_UL_FILE.format(mention)
+    description = Translation.CUSTOM_CAPTION_UL_FILE
     start = datetime.now()
     await update.message.edit_caption(
         caption=Translation.DOWNLOAD_START,
@@ -85,13 +83,14 @@ async def ddl_call_back(bot, update):
                 youtube_dl_url,
                 download_directory,
                 update.message.chat.id,
-                update.id,
+                update.message.id,
                 c_time
             )
-        except asyncio.TimeOutError:
-            await update.message.edit_caption(
-                caption=Translation.SLOW_URL_DECED,
-                parse_mode=enums.ParseMode.HTML
+        except asyncio.TimeoutError:
+            await bot.edit_message_text(
+                text=Translation.SLOW_URL_DECED,
+                chat_id=update.message.chat.id,
+                message_id=update.message.id
             )
             return False
     if os.path.exists(download_directory):
@@ -219,13 +218,14 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         content_type = response.headers["Content-Type"]
         if "text" in content_type and total_length < 500:
             return await response.release()
-        await update.message.edit_caption(
-         
-      
-            caption="""Initiating Download
+        await bot.edit_message_text(
+            chat_id,
+            message_id,
+            text="""Initiating Download
+            
 **🔗 Uʀʟ :** `{}`
-**🗂️ Sɪᴢᴇ :** {}""".format(url, humanbytes(total_length)),
-            parse_mode=enums.ParseMode.HTML
+
+**🗂️ Sɪᴢᴇ :** {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -244,7 +244,7 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                        current_message = """**DᴏᴡɴʟᴏᴀᴅɪɴG**
+                        current_message = """**Download Status**
 **🔗 Uʀʟ :** `{}`
 
 **🗂️ Sɪᴢᴇ :** {}
@@ -258,10 +258,11 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
     TimeFormatter(estimated_total_time)
 )
                         if current_message != display_message:
-                            await update.message.edit_caption(
-                                caption=current_message,
-                                parse_mode=enums.ParseMode.HTML
-            )
+                            await bot.edit_message_text(
+                                chat_id,
+                                message_id,
+                                text=current_message
+                            )
                             display_message = current_message
                     except Exception as e:
                         logger.info(str(e))
