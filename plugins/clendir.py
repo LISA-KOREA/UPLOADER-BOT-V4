d661922567cd0f332338ccdd2271e93162eca560
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-04-27 10:51:49 +0530

    Update clendir.py

--- plugins/clendir.py
+++ plugins/clendir.py
@@ -1,1 +1,12 @@
+#import os
 
+#async def clendir(directory):
+
+    #try:
+        #os.remove(directory)
+    #except:
+       # pass
+  #  try:
+      #  shutil.rmtree(directory)
+   # except:
+      #  pass
