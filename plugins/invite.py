abe51956abb07673c64aa85751005e4f6a6077a7
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-12 16:52:42 +0530

    Update invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
-from plugins.config import BOT_USERNAME
+from plugins.config import Config
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 @Client.on_message(filters.private & filters.command(["invite"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("📡 Sʜᴀʀᴇ Yᴏᴜʀ Lɪɴᴋ" ,url=f"https://t.me/share/url?url=https://t.me/{BOT_USERNAME}?start={message.from_user.id}") ]   ])
+       		[ [ InlineKeyboardButton("📡 Sʜᴀʀᴇ Yᴏᴜʀ Lɪɴᴋ" ,url=f"https://t.me/share/url?url=https://t.me/{Config.BOT_USERNAME}?start={message.from_user.id}") ]   ])
     await message.reply_text(f"**INVITE YOUR FRIENDS**",reply_markup=reply_markup,)
 
