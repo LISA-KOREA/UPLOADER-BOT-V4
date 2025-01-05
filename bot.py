ae5eb011f96b106fbddb55a615f164001ff64618
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2025-01-05 14:09:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@
 
 # [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UPLOADER-BOT-V4
 
-
-
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,8 +9,9 @@ logger = logging.getLogger(__name__)
 
 import os
 from plugins.config import Config
-from pyrogram import filters, Client, idle
 
+from pyrogram import Client as Ntbots
+from pyrogram import filters
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
@@ -21,33 +20,12 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    bot = Client(
+    Ntbots = Ntbots(
         "URL UPLOADER BOT",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
-    )
+        plugins=plugins)
 
-
-    user = Client(
-        "User",
-        session_string=Config.SESSION_STR,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH
-    )
-
-    bot.start()
     print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
-  
-    user.start()
-    print("👤 User client is running!")
-  
-    try:
-        Client.idle()
-    except KeyboardInterrupt:
-        print("Bot is shutting down...")
-      
-    user.stop()
-    bot.stop()
-     
+    Ntbots.run()
