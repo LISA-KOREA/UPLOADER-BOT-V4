dd1952c795269d4c7a03e753e035c426537dafb6
Rajesh R Mahar <136462044+rajrmahar@users.noreply.github.com>
2025-02-05 19:05:16 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,9 +11,9 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7892992528:AAEJveb5Bkqp1W4imGFgzpJj1-1XtIiwDxw")
+    API_ID = int(os.environ.get("API_ID", "28428156"))
+    API_HASH = os.environ.get("API_HASH", "66ebf76c93ebe378996542ff55d32dbe")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 2194304000
@@ -21,7 +21,7 @@ class Config(object):
     SESSION_STR = ""
     FREE_USER_MAX_FILE_SIZE = 2194304000
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://imgur.com/a/sB08mCI")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     
     OUO_IO_API_KEY = ""
@@ -31,16 +31,17 @@ class Config(object):
 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "
+mongodb+srv://pcrmahar:UQ15wLJDo1Q36kWh@cluster0.tnmye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002327840362"))
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "618670084"))
     SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002327840362")
 
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "rajtxtdc4bot")
     ADL_BOT_RQ = {}
 
     # Set False off else True
