ea4f39f5d41560dd49451d7dea2c3e77931ae29f
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2025-12-27 12:22:39 +0530

    Create my-details.py

---
+++ plugins/my-details.py
@@ -0,0 +1,15 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/UnZip-Bot
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UnZip-Bot
+
+# My Telegram Channel :- @NT_BOT_CHANNEL
+
+# Support Group :- @NT_BOTS_SUPPORT
+
+# My Youtube Channel :- https://youtube.com/@ntbot?si=TJaNuy4e1HqWirqQ
+
+# Developers Usernames :- @JOBIN_JUSTIN , @LISA_FAN_LK
+
+# (C) Don’t Change And Edit This File ✍️ | Warning ⚠️
+
+# ©️ 2022 - 2026
